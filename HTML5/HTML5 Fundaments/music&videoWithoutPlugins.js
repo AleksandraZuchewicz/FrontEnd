1ca6e4bcb44a7195c15c65cfda62ade4b3f92701
Aleksandra Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-12-09 12:11:27 +0000

    music and video in HTML

---
+++ HTML5/HTML5 Fundaments/music&videoWithoutPlugins.js
@@ -0,0 +1,24 @@
+//What is HTML5 Media?
+// consisat at least 3 parts:
+// 1. A native media element - audio or video tag user in HTML markup to host a media
+// 2. Different media types
+// 3. Speech API
+
+{
+  /* <video controls loop preload poster="img/kitchen.png" width="900">
+  <source src="media/kitchen.mp4"><!-- Serve first for iPad -->
+  <source src="media/kitchen.ogv">
+  <source src="media/kitchen.webm">
+  <p>This browser does not support native video</p>
+</video>
+
+<audio controls>
+  <source src="media/PolymorphicPodcast-clip.mp3">
+  <source src="media/PolymorphicPodcast-clip.ogg">
+  <source src="media/PolymorphicPodcast-clip.wav">
+  <p>This browser does not support native audio</p>
+</audio> */
+}
+
+//Dynamic content: as the video ends, it automatically starts the next clip
+// it can be used for build-in ads or playlist
