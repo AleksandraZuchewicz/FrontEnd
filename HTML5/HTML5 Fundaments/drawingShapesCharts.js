89f900d8a1cf21fed95bc8435c15093588e47cd4
Aleksandra Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-12-16 14:09:45 +0000

    Chenged file extension from .js to .txt for notes

--- HTML5/HTML5 Fundaments/drawingShapesCharts.js
+++
@@ -1,8 +0,0 @@
-//Types of Images:
-//1. Vector Image: type of image often associated with Adobe Illustrator. Illustration is rendered on a fly when the image is displayed
-// Benefits: you can scale the image, without any size that you want without losing resolution.
-// 2. Raster Image are like JPEGs or PNGs or Gifs. Every pixel has its own designated colour. when increasing the size, image is losing fidelity,
-//as the dimensions are stretched
-
-//What is canvas:
-//HTML Element and JavaScript API
