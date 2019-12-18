b47e72238af45c1a2364b8809fc778124c73cc27
Aleksandra Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-12-18 16:04:28 +0000

    Added keypress event, code refactoring

--- HTML5/TheComplateWebDeveloperCourse_Udemy/DOM/shoppingList/script.js
+++ HTML5/TheComplateWebDeveloperCourse_Udemy/DOM/shoppingList/script.js
@@ -2,12 +2,24 @@ let button = document.getElementById("enter");
 let input = document.getElementById("userinput");
 let ul = document.querySelector("ul")
 
-button.addEventListener("click", function() {
-    if (input.value.length > 0) {
-        let li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value))
-        ul.appendChild(li)
-        input.value = ""
-    }
+function inputLength() {
+    return input.value.length
+}
 
+function createListElement() {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value))
+    ul.appendChild(li)
+    input.value = ""
+}
+button.addEventListener("click", function() {
+    if (inputLength() > 0) {
+        createListElement()
+    }
+})
+
+input.addEventListener("keypress", function(event) {
+    if (inputLength() > 0 && event.keyCode == 13) {
+        createListElement()
+    }
 })
