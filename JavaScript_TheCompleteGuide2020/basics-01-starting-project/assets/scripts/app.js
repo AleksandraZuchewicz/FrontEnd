45acf09177fe8371040432799ed78cf61c4b1ed3
aleksandra.zuchewicz <aleksandra.zuchewicz@gmail.com>
2020-01-20 15:32:17 +0000

    added function for subtract, devided and multiply

--- JavaScript_TheCompleteGuide2020/basics-01-starting-project/assets/scripts/app.js
+++ JavaScript_TheCompleteGuide2020/basics-01-starting-project/assets/scripts/app.js
@@ -1,12 +1,46 @@
 const defaultResultValue = 0;
 let currentResult = defaultResultValue;
 
-function add() {
-    const enteredNumber = parseInt(usrInput.value)
-    const calcDescryption = `${currentResult}+${enteredNumber}`
-    currentResult = currentResult + enteredNumber;
+function getUserNumberInput() {
+    return parseInt(usrInput.value)
+}
+
+function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
+    const calcDescryption = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescryption);
 }
 
 
-addBtn.addEventListener("click", add)
+function add() {
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteLog("+", initialResult, enteredNumber)
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteLog("-", initialResult, enteredNumber)
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteLog("*", initialResult, enteredNumber)
+}
+
+function devide() {
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteLog("/", initialResult, enteredNumber)
+}
+
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+devideBtn.addEventListener("click", devide);
