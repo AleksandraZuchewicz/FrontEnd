e5f5a461f9172d13da32f2f870d5121c332be9f8
aleksandra.zuchewicz <aleksandra.zuchewicz@gmail.com>
2020-01-21 16:48:19 +0000

    Initial Commit - Monster Kill Play

---
+++ JavaScript_TheCompleteGuide2020/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -0,0 +1,34 @@
+const ATTACK_VALUE = 10;
+const MONSTER_ATTACT = 14;
+const STRONG_ATTACK_VALUE = 17;
+
+let maximumLife = 100;
+let currentMonsterHealth = maximumLife;
+let currentPlayerHealth = maximumLife;
+adjustHealthBars(maximumLife);
+
+function attacktFuntion(valueAttack) {
+    const damage = dealMonsterDamage(valueAttack);
+    currentMonsterHealth -= damage;
+    const playerDemage = dealPlayerDamage(MONSTER_ATTACT);
+    currentPlayerHealth -= playerDemage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert("You Won !!!");
+    } else if (currentPlayerHealth <= 0) {
+        alert("You lost!");
+    } else if (currentPlayerHealth <= 0 && currentPlayerHealth <= 0) {
+        alert("You have a drow!");
+
+    }
+}
+
+function attackHandler() {
+    attacktFuntion(MONSTER_ATTACT)
+}
+
+function strongAttackHandler() {
+    attacktFuntion(STRONG_ATTACK_VALUE)
+}
+
+attackBtn.addEventListener("click", attackHandler)
+strongAttackBtn.addEventListener("click", strongAttackHandler)
